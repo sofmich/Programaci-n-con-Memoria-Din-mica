d4858c31ca7906e8514c1bb4c6576b8b62d88bfe
ie714715 <39529828+sofmich@users.noreply.github.com>
2018-06-20 18:18:55 -0500

    Sesion 5 matrices

---
+++ matrices.c
@@ -0,0 +1,23 @@
+/*
+ * matrices.c
+ *
+ *  Created on: 28/05/2018
+ *      Author: ie714715
+ */
+#include <stdlib.h>
+#include <stdio.h>
+void sumaMatrices(int m1[3][3], int m2[3][3], int mr[3][3]);
+void multiplicaMatrices(int m1[3][3], int m2[3][3], int mr[3][3]);
+int main(){
+	int m1[][3]={1,2,3,4,5,6,7,8,9};
+	int m2[][3]={9,8,7,6,5,4,3,2,1};
+
+	return 0;
+}
+void sumaMatrices(int m1[3][3], int m2[3][3], int mr[3][3]){
+
+}
+void multiplicaMatrices(int m1[3][3], int m2[3][3], int mr[3][3]){
+
+}
+
